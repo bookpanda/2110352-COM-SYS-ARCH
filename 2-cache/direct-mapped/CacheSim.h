92be7d157e9d3f09fd6977f2fac583329c03d469
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-25 14:32:34 +0700

    m

--- 2-cache/direct-mapped/CacheSim.h
+++ 2-cache/direct-mapped/CacheSim.h
@@ -12,9 +12,22 @@
 #define CACHESIM_H_INCLUDED
 
 /* Please adjust parameters here */
-#define CACHE_SIZE 1024
-#define INDEX_SIZE 256
-#define BLOCK_SIZE 4
+// #define CACHE_SIZE 1024
+
+// #define CACHE_SIZE 4096
+// #define CACHE_SIZE 8192
+// #define CACHE_SIZE 16384
+#define CACHE_SIZE 32768
+
+// #define BLOCK_SIZE 4
+// #define BLOCK_SIZE 8
+// #define BLOCK_SIZE 16
+#define BLOCK_SIZE 32
+
+// #define INDEX_SIZE 256
+#define INDEX_SIZE 512
+// #define INDEX_SIZE CACHE_SIZE/BLOCK_SIZE
+
 #define TAGLEN 22
 #define INDEXLEN 8
 #define OFFSETLEN 2
--- 2-cache/direct-mapped/README.md
+++ 2-cache/direct-mapped/README.md
@@ -2,4 +2,7 @@
 ```bash
 gcc CacheSim.c -o CacheSim 
 ./CacheSim gcc_ld_trace.txt
+
+gcc CacheSim.c -o ./compiled/CacheSim_b4_c4k
+./compiled/CacheSim_b4_c4k gcc_ld_trace.txt
 ```
